a0971843c1d70f4b41e489f194b9c715fa4c578e
Laura TUFO <laura.tufo94@gmail.com>
2024-05-19 16:24:45 +0200

    Add - Function updateSlide - For images navigation

--- assets/script.js
+++ assets/script.js
@@ -23,14 +23,37 @@ let currentIndex = 0;
 const leftArrow = document.querySelector(".arrow_left");
 const rightArrow = document.querySelector(".arrow_right");
 const dotsContainer = document.querySelector(".dots");
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
 
 leftArrow.addEventListener("click", () => {
   console.log("Left arrow clicked");
+  updateSlide("previous");
 });
 
 rightArrow.addEventListener("click", () => {
   console.log("Right arrow clicked");
+  updateSlide("next");
 });
+
+function updateSlide(action) {
+  if (action === "previous") {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = slides.length - 1;
+    }
+  } else if (action === "next") {
+    if (currentIndex < slides.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+  }
+  const currentSlide = slides[currentIndex];
+  bannerImg.src = `./assets/images/slideshow/${currentSlide.image}`;
+  bannerText.innerHTML = currentSlide.tagLine;
+}
 
 function createDots() {
   for (let index = 0; index < slides.length; index++) {
